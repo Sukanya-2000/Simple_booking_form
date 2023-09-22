b2173ed10dd48fc2283bdaa83b55f49409424d8a
Sukanya-2000 <bhattacharyyasukanya5@gmail.com>
2023-09-22 23:01:00 +0530

    first

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const bookingForm = document.getElementById("bookingForm");
 const displayDetails = document.getElementById("displayDetails");
 
 // Add event listener for form submission
-bookingForm.addEventListener("submit", function (e) {
+bookingForm.addEventListener("submit", async function (e) {
   e.preventDefault();
 
   // Get input values
@@ -11,12 +11,33 @@ bookingForm.addEventListener("submit", function (e) {
   const email = document.getElementById("email").value;
   const phone = document.getElementById("phone").value;
 
-  // Create a paragraph element to display the details
-  const detailsParagraph = document.createElement("p");
-  detailsParagraph.innerHTML = `<strong>Name:</strong> ${name}<br><strong>Email:</strong> ${email}<br><strong>Phone:</strong> ${phone}`;
+  // Create a data object to send in the POST request
+  const data = {
+    name,
+    email,
+    phone,
+  };
 
-  // Append the paragraph to the display div
-  displayDetails.appendChild(detailsParagraph);
+  try {
+    // Make a POST request to the cloud API
+    const response = await axios.post("https://crudcrud.com/api/8f8c54a8b9be4da5842c7d0db09f8488", data);
+
+    // Check if the request was successful
+    if (response.status === 201) {
+      // Display a success message
+      const detailsParagraph = document.createElement("p");
+      detailsParagraph.textContent = "Booking details saved successfully!";
+      displayDetails.appendChild(detailsParagraph);
+    } else {
+      // Display an error message
+      const detailsParagraph = document.createElement("p");
+      detailsParagraph.textContent = "Failed to save booking details.";
+      displayDetails.appendChild(detailsParagraph);
+    }
+  } catch (error) {
+    // Handle any errors that occurred during the request
+    console.error("Error:", error);
+  }
 
   // Clear the form inputs
   bookingForm.reset();
